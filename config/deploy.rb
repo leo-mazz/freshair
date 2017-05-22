e66089d2716a0bb8b80f7ea54fad47c27c338201
Leonardo Mazzone <hello@leomazzone.me>
2017-05-22 15:32:53 +0100

    Update capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,11 @@ set :repo_url, "https://leo-mazz:nonSAPREIproprio12@github.com/leo-mazz/refresh-
 set :deploy_to, "/var/www/refresh"
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
+# append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "vendor/bundle"
 
-after :finishing, :restart_apache do
-  on roles(:web) do
-    invoke_command "/etc/init.d/apache2 restart", :via => run_method
-  end
-end
 
 
 # The following lines are required because we can't directly access our Virtual
