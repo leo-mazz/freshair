70ee169fce9cc96a51a79db24378ae3dce3adf53
Leonardo Mazzone <hello@leomazzone.me>
2017-05-22 13:17:03 +0100

    Adapt to shows without a picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
     @static_pages = Page.all.order(priority: :desc)
     # TODO: change these
     @on_air = true
-    @current_show = Show.first
+    @current_show = Show.last
   end
 end
