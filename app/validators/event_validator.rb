7ce8a237db39bc00eb3d44213fd07a5893dff604
Leonardo Mazzone <hello@leomazzone.me>
2017-06-13 02:13:14 +0200

    Use .blank? instead of other silliness part II

--- app/validators/event_validator.rb
+++ app/validators/event_validator.rb
@@ -1,11 +1,11 @@
 class EventValidator < ActiveModel::Validator
 
   def validate(record)
-    unless (record.facebook_event.starts_with? 'https://') || (record.facebook_event.starts_with? 'http://') || (record.facebook_event == '')
+    unless (record.facebook_event.starts_with? 'https://') || (record.facebook_event.starts_with? 'http://') || (record.facebook_event.blank?)
       record.errors[:facebook_event] << "This doesn't look like a valid URL"
     end
 
-    unless (record.link_to_tickets.starts_with? 'https://') || (record.link_to_tickets.starts_with? 'http://') || (record.link_to_tickets == '')
+    unless (record.link_to_tickets.starts_with? 'https://') || (record.link_to_tickets.starts_with? 'http://') || (record.link_to_tickets.blank?)
       record.errors[:link_to_tickets] << "This doesn't look like a valid URL"
     end
   end
