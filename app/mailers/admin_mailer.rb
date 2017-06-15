7eb08d719604a03d24a18e920ce7eb22c6baf980
Leonardo Mazzone <hello@leomazzone.me>
2017-06-15 20:48:13 +0200

    Use webmaster email, not my own

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,8 +2,7 @@ class AdminMailer < ApplicationMailer
 
   def new_user_notification(user)
     @user = user
-    # TODO: remember to change this in production (make it an ENV variable)
-    mail(to: 'leo.mazzone15@gmail.com', subject: 'New user pending')
+    mail(to: ENV['WEBMASTER_EMAIL'], subject: 'New user pending')
   end
 
 end
