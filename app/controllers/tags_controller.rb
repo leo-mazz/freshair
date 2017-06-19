bacecfbb01fc8291456982f4d4bc409eb457a21d
Leonardo Mazzone <hello@leomazzone.me>
2017-06-19 20:45:35 +0200

    Improve the appearance of small things, and podcasts

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,7 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.friendly.find(params[:id])
+    @title = @tag.name
     @tag_posts = @tag.posts.order(created_at: :desc)
   end
 
--- app/helpers/uri_helper.rb
+++ app/helpers/uri_helper.rb
@@ -6,6 +6,6 @@ module UriHelper
   end
 
   def mixcloud(uri)
-    '<iframe width="100%" height="120" src="https://www.mixcloud.com/widget/iframe/?feed=' + escape(uri) + '&hide_cover=1&hide_artwork=1" frameborder="0"></iframe>'
+    '<iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?feed=' + escape(uri) + '&hide_cover=1&light=1&hide_artwork=1" frameborder="0"></iframe>'
   end
 end
