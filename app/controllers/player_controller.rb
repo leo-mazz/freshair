e277058e1717d1ab9cce249508f193e6bc9cf9ed
Leonardo Mazzone <hello@leomazzone.me>
2017-07-03 19:57:22 +0200

    Allow embedding standalone player

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,6 +2,8 @@ class PlayerController < ApplicationController
   layout 'player'
 
   def listen
+    # Allow embedding on other websites
+    response.headers.delete "X-Frame-Options"
   end
 
 end
