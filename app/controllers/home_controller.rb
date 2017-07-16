d8a3bf4b030fa2506d070e08e40b971560ae2e02
Leonardo Mazzone <hello@leomazzone.me>
2017-07-16 15:35:29 +0200

    Fix small mistake

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @latest_posts = Post.order(created_at: :desc).limit(6)
-    @highlights = HighlightsService.get_highlights[0..1]
+    @highlights = HighlightsService.get_highlights
     @post_types = Tag.post_types
   end
 
