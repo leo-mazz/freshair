89ae7ce976f4ed06914e26582c044c2fd0337ec1
Leonardo Mazzone <hello@leomazzone.me>
2017-08-09 01:13:50 +0100

    Try to fix ckeditor not working in production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( listen.js )
+# https://github.com/galetahub/ckeditor/issues/387
+Rails.application.config.assets.precompile += %w( ckeditor/* )
