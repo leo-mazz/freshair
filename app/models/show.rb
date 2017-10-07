98f404e001defb6f727ab0ca340cd1e6e608d464
Leonardo Mazzone <hello@leomazzone.me>
2017-10-07 12:27:43 +0100

    Get broadcast times in a less idiotic way

--- app/models/show.rb
+++ app/models/show.rb
@@ -34,16 +34,16 @@ class Show < ApplicationRecord
 
     return if current_schedule.nil?
 
-    current_schedule.assignments.each do |assignment|
-      if assignment.show.id == self.id
-        day_time = {}
-        day_time[:day] = assignment.day_name
-        day_time[:start] = assignment.start_time.strftime('%H:%M')
-        day_time[:end] = assignment.end_time.strftime('%H:%M')
+    assignment = current_schedule.assignments.where(show_id: self.id).first
+    return if assignment.nil?
 
-        day_times << day_time
-      end
-    end
+    day_time = {}
+    day_time[:day] = assignment.day_name
+    day_time[:start] = assignment.start_time.strftime('%H:%M')
+    day_time[:end] = assignment.end_time.strftime('%H:%M')
+
+    day_times << day_time
+
 
     # TODO: change this to allow multiple broadcast times to be returned. But
     # careful, will break a few things (expecting a single result to be returned)
