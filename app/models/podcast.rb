4cef004892a0c9fb94657f4383b4965200ccb3fe
Leonardo Mazzone <hello@leomazzone.me>
2017-10-09 15:54:31 +0100

    Allow creating podcasts without broadcast date

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -4,7 +4,7 @@ class Podcast < ApplicationRecord
   # https://stackoverflow.com/questions/16782990/rails-how-to-populate-parent-object-id-using-nested-attributes-for-child-obje
   has_many :played_tracks, :inverse_of => :podcast
 
-  validates_presence_of :show, :uri, :broadcast_date
+  validates_presence_of :show, :uri
   validates_with PodcastValidator
 
   accepts_nested_attributes_for :played_tracks, :allow_destroy => true
